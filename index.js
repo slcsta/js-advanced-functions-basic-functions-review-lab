ed2a3bfa6b8ec57382abde2e9652b615cff77f94
Sarah <sarahlchavez@gmail.com>
2021-09-21 13:16:43 -0700

    commit tree ded99b53fe9577ba88b08f956a68ba59669ef136

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Your code here
+function saturdayFun(activity="roller-skate") {
+    console.log(`This Saturday, I want to ${activity}!`);
+    return `This Saturday, I want to ${activity}!`;
+}
+
+
+// LAB: Implement a function expression called mondayWork. 
+// The function should return a String like "This Monday, I will ... ." 
+// Fill in the ... with the activity that's passed in as the first parameter. 
+// If nothing is passed in, default to "go to the office". 
+// Run learn to verify you've gotten this set of tests passing before continuing with the lesson.
+
+const mondayWork = function(activity="go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+
+// write a function called wrapAdjective
+// wrapAdjective should return a function:
+    //an innner function
+    //takes a single parameter that defaults to "special"
+function wrapAdjective(flair="*"){
+    return function(quality="special"){
+        return `You are ${flair}${quality}${flair}!`;
+    }
+}
+
+let Calculator = {
+    add: function(a, b){
+        return a + b;
+    },
+   subtract: function(a, b){
+        return a - b;
+   },
+   multiply: function(a, b){
+    return a * b;
+   },
+   divide: function(a, b){
+    return a/b;
+   }
+}
+
+function actionApplyer(startingInteger, arrayFunctions){
+    for (let i = 0; i < arrayFunctions.length; i++) {
+        startingInteger = arrayFunctions[i](startingInteger);
+    }
+    return startingInteger;
+}
+
